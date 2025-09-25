eb173bdd2650eb72bf9a622d9b8654ba9412bd68
Ryan-Kozel <133525567+Ryan-Kozel@users.noreply.github.com>
2025-09-25 12:03:54 -0700

    Update accounts.py
    Added comments and doc strings

--- banking/accounts.py
+++ banking/accounts.py
@@ -8,7 +8,8 @@ class BankAccount:
         self.balance = initial_balance
 
     def deposit(self, amount):
-        if amount <= 0:
+        """Deposit an amount into an account"""
+        if amount <= 0: #make sure inputed amout is positive
             raise InvalidAmountError("Deposit must be positive", 200)
         else:
             self.balance = self.balance + amount
@@ -17,7 +18,7 @@ class BankAccount:
         raise NotImplementedError
 
     def __str__(self):
-        string = "Account Owner: {name}, Balance: ${bal:.2f}".format(name=self.owner_name, bal=self.balance)
+        string = "Account Owner: {name}, Balance: ${bal:.2f}".format(name=self.owner_name, bal=self.balance) #format to nice output message
         return string
 
 
@@ -26,10 +27,15 @@ class SavingAccount(BankAccount):
         super().__init__(owner_name, initial_balance)
 
     def withdraw(self, amount):
+        """ 
+        Withdraw an amount from a savings account
+        Savings accounts have withdraw fee of $2.00
+        """
+        
         fee = 2.00
-        if amount < 0:
+        if amount < 0: #make sure that withdraw amount is positive
             raise InvalidAmountError("Deposit must be positive", 200)
-        elif amount + fee > self.balance:
+        elif amount + fee > self.balance: #account cannot go into negatives
             raise InsufficientFundsError("Insufficient funds", 201)
         else:
             self.balance = self.initial_balance - amount - fee
@@ -40,10 +46,14 @@ class CheckingAccount(BankAccount):
         super().__init__(owner_name, initial_balance)
 
     def withdraw(self, amount):
+        """ 
+        Withdraw an amount from a checking account
+        Savings accounts have withdraw fee of $1.00
+        """
         fee = 1.00
-        if amount < 0:
+        if amount < 0: #make sure withdraw amount positive
             raise InvalidAmountError("Deposit must be positive", 200)
-        elif amount + fee > self.balance:
+        elif amount + fee > self.balance: #account cannot go into negatives
             raise InsufficientFundsError("Insufficient funds", 201)
         else:
             self.balance = self.balance - amount - fee
