feec1e695547bc5ff166812b3493ed7e96281ea6
Ryan-Kozel <133525567+Ryan-Kozel@users.noreply.github.com>
2025-09-25 12:14:55 -0700

    Update persistence.py
    Added comments and doc strings

--- banking/persistence.py
+++ banking/persistence.py
@@ -8,12 +8,16 @@ class Bank:
         self.accounts = {}
         self.shelf = db_path
 
-    # TODO
     def create_account(self, account_type, owner_name, initial_balance=0.0):
+        """ 
+        Create either a savings account or a checking account depending on account_type
+        """
+        #If account with owner's name already exists, print an error message and return False
         if owner_name in self.accounts:
             print(f"Account: {owner_name} already found")
             return False
         else:
+        #If it does not exist, create it based on specified type
             if account_type.lower() == 'checking':
                 acc = CheckingAccount(owner_name, initial_balance)
                 self.accounts[acc.owner_name] = acc
@@ -24,17 +28,27 @@ class Bank:
                 return True
 
     def get_account(self, owner_name):
+        """
+        Find account stored in accounts dictionary, or return None if not found
+        """
         if owner_name in self.accounts:
             return self.accounts[owner_name]
         else:
             return None
 
     def save_data(self):
+        """
+        Store the accounts dictionary in a shelve file
+        """
         with shelve.open(self.shelf) as s:
             s['accounts'] = self.accounts
 
     def load_data(self):
+        """
+        Retrieve the accounts dictionary, returning an empty dictionary if the file does not exist
+        """
         with shelve.open(self.shelf) as s:
+            #Get the accounts dictionary, defaut value is an empty dictionary if not found
             self.accounts = s.get('accounts', {})
             return self.accounts
 
